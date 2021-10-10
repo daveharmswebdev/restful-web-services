3ec5a7fa0e51da700fd0448794a3f84966fab007
Dave Harms <daveharmswebdev@gmail.com>
2021-10-10 06:47:24 -0500

    added jpa

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values(10001, sysdate(), 'test1');
+insert into user values(10002, sysdate(), 'test2');
+insert into user values(10003, sysdate(), 'test3');
+insert into user values(10004, sysdate(), 'test4');
